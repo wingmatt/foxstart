0157e463613359eaa77900881d57b1fd8304f814
wingmatt <matt@wingwebhelp.com>
2020-08-15 00:51:33 -0700

    Use DayJS to format dateTime. Add month data

--- clock.js
+++ clock.js
@@ -1,16 +1,20 @@
 const getDateTime = () => {
-  let dateTime = new Date();
-  let date = dateTime.toDateString();
-  let time = dateTime.toTimeString();
+  let now = dayjs();
+  let month = now.format("MMM");
+  let date = now.format("D");
+  let time = now.format("h:mm");
   return {
+    month: month,
     date: date,
     time: time,
   };
 };
 
 const showDateTime = (dateTime) => {
+  const monthContainer = document.getElementById("clock-month");
   const dateContainer = document.getElementById("clock-date");
   const timeContainer = document.getElementById("clock-time");
+  monthContainer.innerText = dateTime.month;
   dateContainer.innerText = dateTime.date;
   timeContainer.innerText = dateTime.time;
 };
